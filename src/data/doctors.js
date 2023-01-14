8c60268dff9ff81613102a5abb037b90e9eede93
galapsa <sadisha_galappatti@manulife.com>
2023-01-13 23:47:41 -0500

    adding data to use

---
+++ src/data/doctors.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: "1",
+    name: "Frank Waiton",
+  },
+  {
+    id: "2",
+    name: "Phil Mckenzie",
+  },
+  {
+    id: "3",
+    name: "Jen Frye",
+  },
+  {
+    id: "4",
+    name: "Craig Helbling",
+  },
+  {
+    id: "5",
+    name: "Karen Macarthur",
+  },
+  {
+    id: "6",
+    name: "Alan Gurniak",
+  },
+  {
+    id: "7",
+    name: "Jose Herrera",
+  },
+  {
+    id: "8",
+    name: "Robert Mcsweeney",
+  },
+  {
+    id: "9",
+    name: "Rosalina Guerriero",
+  },
+  {
+    id: "10",
+    name: "Tim Olsen",
+  },
+];
